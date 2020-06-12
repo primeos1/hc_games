098b4b8f89b786279eec0a57781fae6fc1d69fe3
primeos1 <akhenisio@yahoo.com>
2020-06-12 08:20:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { Grid, Typography, Paper, Divider, Button } from '@material-ui/core';
+import { Form, Info, Progress } from './components';
+import { getInitialState, getFeedback } from './util';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = getInitialState();
+
+  resetGame = () => this.setState(getInitialState());
+
+  updateAppState = (guess) => {
+    const { actual } = this.state;
+
+    const absDiff = Math.abs(guess - actual);
+    const { feedbackMessage, feedbackColor } = getFeedback(absDiff);
+
+    this.setState(prevState => ({
+      guess,
+      allGuesses: [...prevState.allGuesses, { guess, feedbackColor }],
+      attempt: prevState.attempt + 1,
+      feedbackMessage,
+      block: absDiff === 0,
+    }));
+  }
+
+  render() {
+    const { allGuesses, feedbackMessage, block, attempt, show } = this.state;
+
+    const guessList = allGuesses.map((item, index) => (
+      <li key={index}>
+        <span>{item.guess}</span>
+      </li>
+    ));
+
+    return (
+      <Grid style={{ height: '100vh' }} justify="center" alignItems="center" container>
+        <Grid item xs={3}>
+          <Paper style={{ padding: '50px' }} elevation={6}>
+            <Typography align="center" variant="h2" gutterBottom>OSE GAME OF HOT or COLD</Typography>
+            <Divider style={{ margin: '20px 0' }} />
+            <div className={`feedback ${feedbackMessage[0].toLowerCase()}`}>
+              <h2 className="feedback-text">{feedbackMessage}</h2>
+            </div>
+            <Form block={block} returnGuessToApp={value => this.updateAppState(value)} />
+            <Progress feedbackMessage={feedbackMessage} attempt={attempt} guessList={guessList} />
+            <Button style={{ marginBottom: '15px' }} fullWidth variant="contained" color="primary" onClick={this.resetGame}>Reset Game</Button>
+            <Info show={show} onClose={this.handleClose} />
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
